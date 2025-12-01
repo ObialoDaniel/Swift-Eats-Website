90dbf425d012e0fa0a7cdb8470470e1ed575c78b
otunaboEmmanuel <otunabochidubem@gmail.com>
2025-12-01 18:14:18 +0100

    fixing dTO naming

--- swift-eats/src/main/java/com/hc/swifteats/dto/UserRequest.java
+++ swift-eats/src/main/java/com/hc/swifteats/dto/UserRequest.java
@@ -1,5 +1,7 @@
 package com.hc.swifteats.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +10,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequest {
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @Email(message = "provide a valid email")
     private String email;
+    @NotNull
     private String password;
+    @NotNull
     private String phoneNumber;
+    @NotNull
     private String address;
 }
